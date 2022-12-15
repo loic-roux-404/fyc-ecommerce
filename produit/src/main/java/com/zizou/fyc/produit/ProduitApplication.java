007bfbefb433d250e8901561178dcb1d23e85230
RayaneKettana <rkettana@gmail.com>
2022-12-15 17:50:59 +0100

    Mise en place du microservice Produit

---
+++ produit/src/main/java/com/zizou/fyc/produit/ProduitApplication.java
@@ -0,0 +1,12 @@
+package com.zizou.fyc.produit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProduitApplication.class, args);
+    }
+    
+}
