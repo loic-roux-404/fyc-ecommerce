65267d6cdd5e8df46bd7639cd88f2b5edd380ab4
RayaneKettana <rkettana@gmail.com>
2022-12-17 12:20:06 +0100

    Mise en place du microservice fraude

---
+++ fraude/src/main/java/com/zizou/fyc/fraude/FraudeApplication.java
@@ -0,0 +1,11 @@
+package com.zizou.fyc.fraude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudeApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FraudeApplication.class, args);
+  }
+}
